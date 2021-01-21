80e0e1ca1ac633aa24ccd310ca940736aa622d4a
Faisal Taher <faisal.taher@jisc.ac.uk>
2021-01-21 12:19:24 +0000

    HTML changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello world, this is a sample app</div>;
+  return (
+    <>
+      <div className="App">Hello world, this is a sample app.</div>)<p>Hello</p>
+    </>
+  );
 }
 
 export default App;
