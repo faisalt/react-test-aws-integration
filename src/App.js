5df487047d0c043f7f92f7e3c8c74598b0128b55
Faisal Taher <faisal.taher@jisc.ac.uk>
2021-01-22 08:56:13 +0000

    Updates text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <div className="App">Hello world, this is a sample app.</div>)
       <p>Hello world</p>
       <p>Some more text</p>
-      <p>Some more text here</p>
+      <p>Some more text here.</p>
     </>
   );
 }
