4d2ba81713ffc6b37f058511162a537b5657a3f4
Faisal Taher <faisal.taher@jisc.ac.uk>
2021-01-25 18:00:11 +0000

    Updates buildspec

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ function App() {
     <>
       <div className="App">Hello world, this is a sample app.</div>)
       <p>Hello world</p>
-      <p>Some more text</p>
-      <p>Some more text</p>
+      <p>We have just updated develop!</p>
     </>
   );
 }
