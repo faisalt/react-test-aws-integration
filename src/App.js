833126069a2a895d30e389833a35a8c5d9dfc019
Faisal Taher <faisal.taher@jisc.ac.uk>
2021-01-25 19:48:00 +0000

    Updates text

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="App">Hello world, this is a sample app.</div>)
+      <div className="App">Hello world, this is a sample app.</div>
       <p>Hello world</p>
       <p>We have just updated develop! again!</p>
     </>
