dcc6e55abe49fc93ef49f9abfd4c7da2626f2952
Faisal Taher <faisal.taher@jisc.ac.uk>
2021-02-05 11:55:59 +0000

    Changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
       <div className="App">Hello world, this is a sample app.</div>
       <p>Hello world</p>
       <p>We have just updated develop! again!</p>
-      <p>We have just updated develop! again!</p>
     </>
   );
 }
